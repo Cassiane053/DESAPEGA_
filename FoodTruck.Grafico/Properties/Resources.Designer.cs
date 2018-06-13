4eab06b09d18fa77f65a1d080d210b9556646f17
Cassiane053 <36904667+Cassiane053@users.noreply.github.com>
2018-06-12 22:32:03 -0300

    correção assemblys, etc

--- FoodTruck.Grafico/Properties/Resources.Designer.cs
+++ FoodTruck.Grafico/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FoodTruck.Grafico.Properties {
+namespace Desapega.Grafico.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace FoodTruck.Grafico.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FoodTruck.Grafico.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Desapega.Grafico.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
